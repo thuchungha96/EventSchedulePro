4341101853272a193333d3bac48b064e181c84fc
ha.thuchung96@outlook.com <ha.thuchung96@outlook.com>
2024-05-18 18:46:52 +0700

    [Modify] Done

--- Pages/ExcelReader.cshtml.cs
+++ Pages/ExcelReader.cshtml.cs
@@ -1,6 +1,9 @@
+using EventSchedulePro.Data.Class;
+using EventSchedulePro.Data.Context;
 using ExcelDataReader;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using MySql.Data.MySqlClient;
 using System.Globalization;
 using System.Net.WebSockets;
@@ -12,6 +15,13 @@ namespace EventSchedulePro.Pages
         private string FileNameExample = "excel";
         [BindProperty]
         public List<List<object>> OutputDat { get; set; }
+
+        public readonly EventDBContext _context;
+        public ExcelReaderModel(EventDBContext context)
+        {
+            _context = context;
+        }
+
         public void OnGet()
         {
             System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
@@ -94,7 +104,7 @@ namespace EventSchedulePro.Pages
             }
             return Page();
         }
-        private void saveFileIntoDatabase(List<List<object>> o)
+        private async void saveFileIntoDatabase(List<List<object>> o)
         {
             for (int i=1; i<o.Count; i++)
             {
@@ -108,19 +118,19 @@ namespace EventSchedulePro.Pages
                 //Check Group not exited then insert
                 string groupFromExcel = o[i][4]?.ToString();
                 if (String.IsNullOrEmpty(groupFromExcel)) groupFromExcel = "All";
-                using (MySqlConnection conn = new MySqlConnection("server=104.238.129.230;uid=jinzschedule;pwd=Metmoivl@123.@124!;database=jinzschedule;connect Timeout=30"))
                 {
-                    conn.Open();
                     {
                         if (!String.IsNullOrEmpty(groupFromExcel))
                         {
                             var groupList = groupFromExcel.Trim().Split(",");
-                            foreach (var group in groupList)
+                            foreach (var item in groupList)
                             {
-                                var groupName = checkGroupExited(conn, group.Trim());
-                                if (string.IsNullOrEmpty(groupName))
+                                var group = _context.Groups.FirstOrDefault(x => x.Name == item.Trim());
+                                if (group == null)
                                 {
-                                    insertGroup(conn, group.Trim());
+                                    Group newItem = new Group { Name = item.Trim(), Detail = item.Trim() };
+                                    _context.Groups.Add(newItem);
+                                    _context.SaveChanges();
                                 }
                             }
                         }
@@ -130,12 +140,14 @@ namespace EventSchedulePro.Pages
                         if (!String.IsNullOrEmpty(o[i][5]?.ToString()))
                         {
                             var StaffList = o[i][5].ToString().Trim().Split(",");
-                            foreach (var staff in StaffList)
+                            foreach (var item in StaffList)
                             {
-                                var staffName = checkStaffExited(conn, staff.Trim());
-                                if (string.IsNullOrEmpty(staffName))
+                                var staff = _context.Staffs.FirstOrDefault(x => x.UserName == item.Trim());
+                                if (staff == null)
                                 {
-                                    insertStaff(conn, staff.Trim());
+                                    Staff newStaff = new Staff { UserName = item.Trim(), GroupID = 0, RoleUser = "1", PasswordHash = Base64Encode("1") };
+                                    _context.Add(newStaff);
+                                    _context.SaveChanges();
                                 }
                             }
                         }
@@ -146,35 +158,41 @@ namespace EventSchedulePro.Pages
                         {
                             var groupList = groupFromExcel.Trim().Split(",");
                             var StaffList = o[i][5].ToString().Trim().Split(",");
-                            List<Group> Groups = getListGroup(conn);
-                            List<Staff> Staffs= getListStaff(conn);
+                            var g = _context.Groups.ToList();
+                            var s = _context.Staffs.ToList();
                             string GroupIds = "";
                             string GroupNames = "";
                             string StaffIds = "";
                             string StaffNames = "";
                             foreach(var item in groupList) {
-                                var group = Groups.FirstOrDefault(x => x.name == item.Trim());
+                                var group = g.FirstOrDefault(x => x.Name == item.Trim());
                                 if (group != null)
                                 {
-                                    GroupIds += group.id + ",";
-                                    GroupNames += group.name + ",";
+                                    GroupIds += group.Id + ",";
+                                    GroupNames += group.Name + ",";
                                 }
                             }
 
                             foreach (var item in StaffList)
                             {
-                                var staff = Staffs.FirstOrDefault(x => x.username == item.Trim());
+                                var staff = s.FirstOrDefault(x => x.UserName == item.Trim());
                                 if (staff != null)
                                 {
-                                    StaffIds += staff.id + ",";
-                                    StaffNames += staff.username + ",";
+                                    StaffIds += staff.Id + ",";
+                                    StaffNames += staff.UserName + ",";
                                 }
                             }
 
                             if (!string.IsNullOrEmpty(GroupIds) && !string.IsNullOrEmpty(GroupNames))
-                            foreach (var staff in StaffList)
+                            foreach (var item in StaffList)
                             {
-                                updatetStaffGroup(conn, staff.Trim(), GroupIds, GroupNames);
+                                    var staff = _context.Staffs.FirstOrDefault(x => x.UserName == item.Trim());
+                                    if (staff != null)
+                                    {
+                                        staff.GroupIds = GroupIds;
+                                        staff.GroupNames = GroupNames;
+                                        _context.SaveChanges();
+                                    }
                             }
                             var time = o[i][0].ToString().Trim().Split("-");
                             string starttime = "";
@@ -193,10 +211,25 @@ namespace EventSchedulePro.Pages
 
                             foreach (var item in groupList)
                             {
-                                var group = Groups.FirstOrDefault(x => x.name == item.Trim());
+                                var group = g.FirstOrDefault(x => x.Name == item.Trim());
                                 if (group != null)
                                 {
-                                    insertSchedule(conn, o[i][1]?.ToString(), group.id.ToString(), o[i][1]?.ToString(), StaffIds, o[i][3]?.ToString(), o[i][2]?.ToString(), o[i][6]?.ToString(), starttime, endtime, StaffNames);
+                                    Schedule t = new Schedule
+                                    {
+                                        Name = o[i][1]?.ToString(),
+                                        Date = DateTime.Now.ToString("yyyy-MM-dd"),
+                                        GroupID = group.Id.ToString(),
+                                        Activity = o[i][1]?.ToString(),
+                                        Staff = StaffIds,
+                                        StudentLeader = o[i][3]?.ToString(),
+                                        Note = o[i][2]?.ToString(),
+                                        Location = o[i][6]?.ToString(),
+                                        FromTime = starttime,
+                                        ToTime = endtime,
+                                        StaffNames = StaffNames
+                                    };
+                                    _context.Schedules.Add(t);
+                                    _context.SaveChanges();
                                 }
                             }
 
@@ -206,108 +239,10 @@ namespace EventSchedulePro.Pages
                 }
             }
         }
-        #region Check Group Exited / Add New Group
-        private string checkGroupExited(MySqlConnection conn, string name)
-        {
-            string groupname = "";
-            string sql = $"SELECT name FROM groupschedule WHERE name = N'{name}'";
-            MySqlCommand cmd = new MySqlCommand(sql, conn);
-            using (MySqlDataReader rdr = cmd.ExecuteReader())
-            {
-                while (rdr.Read())
-                {
-                    groupname += @rdr.GetString(0) + ", ";
-                }
-            }
-            return groupname;
-        }
-        private void insertGroup(MySqlConnection conn, string name)
-        {
-            string myInsertQuery = $"insert into groupschedule(name,detail) values (N'{name}',N'{name}')";
-            MySqlCommand myCommand = new MySqlCommand(myInsertQuery, conn);
-            myCommand.ExecuteNonQuery();
-        }
-        #endregion Check Group Exited / Add New Group
-
-        #region Check Staff Exited / Add New Staff
-        private string checkStaffExited(MySqlConnection conn, string name)
-        {
-            string staffname = "";
-            string sql = $"SELECT username FROM user WHERE username = N'{name}'";
-            MySqlCommand cmd = new MySqlCommand(sql, conn);
-            using (MySqlDataReader rdr = cmd.ExecuteReader())
-            {
-                while (rdr.Read())
-                {
-                    staffname += @rdr.GetString(0) + ", ";
-                }
-            }
-            return staffname;
-        }
-        private void insertStaff(MySqlConnection conn, string username)
-        {
-            string myInsertQuery = $"INSERT INTO user (username,groupid,roleuser,passwordhash) VALUES ('{username}',0,1,'{Base64Encode("1")}')";
-            MySqlCommand myCommand = new MySqlCommand(myInsertQuery, conn);
-            myCommand.ExecuteNonQuery();
-        }
         public static string Base64Encode(string plainText)
         {
             var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
             return System.Convert.ToBase64String(plainTextBytes);
-        }
-        #endregion Check Staff Exited / Add New Staff
-
-        #region Get List Group and Staff
-        private List<Group> getListGroup(MySqlConnection conn)
-        {
-            List<Group> Groups = new List<Group>();
-            string sql = $"SELECT id,name FROM groupschedule";
-            MySqlCommand cmd = new MySqlCommand(sql, conn);
-            using (MySqlDataReader rdr = cmd.ExecuteReader())
-            {
-                while (rdr.Read())
-                {
-                    Groups.Add(new Group{ id = @rdr.GetInt32(0), name = @rdr.GetString(1) });
-                }
-            }
-            return Groups;
-        }
-        private List<Staff> getListStaff(MySqlConnection conn)
-        {
-            List<Staff> staffs = new List<Staff>();
-            string sql = $"SELECT id,username FROM user";
-            MySqlCommand cmd = new MySqlCommand(sql, conn);
-            using (MySqlDataReader rdr = cmd.ExecuteReader())
-            {
-                while (rdr.Read())
-                {
-                    staffs.Add(new Staff { id = @rdr.GetInt32(0), username = @rdr.GetString(1) });
-                }
-            }
-            return staffs;
-        }
-        public class Group
-        {
-            public string name;
-            public int id;
-        }
-        public class Staff
-        {
-            public string username;
-            public int id;
-        }
-        private void updatetStaffGroup(MySqlConnection conn, string username, string Ids, string Names)
-        {
-            string myInsertQuery = $"update user set GroupIds ='{Ids}', GroupNames=N'{Names}' where username = '{username}'";
-            MySqlCommand myCommand = new MySqlCommand(myInsertQuery, conn);
-            myCommand.ExecuteNonQuery();
-        }
-        #endregion Get List Group and Staff
-        private void insertSchedule(MySqlConnection conn, string name,string groupId, string Activity, string staffList, string StudentLeader, string Note, string Location, string from, string to, string staffName)
-        {
-            string myInsertQuery = $"insert into schedule(name,date,groupid,activity,staff,studentLeader,Note,Location,fromtime,totime,StaffNames) values ('{name}','{DateTime.Now.ToString("yyyy-MM-dd")}',{groupId},N'{Activity}',N'{staffList}',N'{StudentLeader}',N'{Note}',N'{Location}',N'{from}',N'{to}',N'{staffName}')";
-            MySqlCommand myCommand = new MySqlCommand(myInsertQuery, conn);
-            myCommand.ExecuteNonQuery();
         }
     }
 
